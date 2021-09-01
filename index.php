461637576eab032e174d355139b6408dffe461ec
John <jonathan.datil@gmail.com>
2021-09-01 14:02:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Premier projet</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light text-light bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Jeff Nys</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        </ul>
+        <span class="navbar-text">
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="test.php">Tests</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="exo.php">Exo</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="notes.php">Notes</a>
+                </li>
+            </ul>
+        </span>
+        </div>
+    </div>
+    </nav>
+    <div class="container">
+        <h1>Le site de Jeff Nys</h1>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+  </body>
+</html>
